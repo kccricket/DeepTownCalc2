b2c89ef0526a09abc6abe11c48f20cf994361134
Keith Constable <kccricket@gmail.com>
2018-07-24 00:32:05 -0400

    switching to cli-plugin-element-ui

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
-import Vue from "vue";
-import App from "@/App.vue";
-import store from "@/store";
-import GameDataPlugin from "@/data/game-data";
+import Vue from 'vue';
+import App from '@/App.vue';
+import store from '@/store';
+import GameDataPlugin from '@/data/game-data';
 
-import ElementUI from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
-import locale from "element-ui/lib/locale/lang/en";
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import locale from 'element-ui/lib/locale/lang/en';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
 
 Vue.use(ElementUI, { locale });
-Vue.component("el-input-number", ElementUI.InputNumber);
-Vue.component("el-select", ElementUI.Select);
-Vue.component("el-option", ElementUI.Option);
-Vue.component("el-optgroup", ElementUI.OptionGroup);
+Vue.component('el-input-number', ElementUI.InputNumber);
+Vue.component('el-select', ElementUI.Select);
+Vue.component('el-option', ElementUI.Option);
+Vue.component('el-optgroup', ElementUI.OptionGroup);
 
 Vue.use(GameDataPlugin);
 
@@ -19,5 +23,5 @@ Vue.config.productionTip = false;
 
 new Vue({
   store,
-  render: h => h(App)
-}).$mount("#app");
+  render: (h) => h(App),
+}).$mount('#app');
