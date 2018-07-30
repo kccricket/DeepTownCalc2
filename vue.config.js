119c94ff8c6ab2b38002534b64d19e8fa0c3053b
Keith Constable <kccricket@gmail.com>
2018-07-30 01:26:03 -0400

    Per element-ui docs, set default en lang.

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,15 @@
+const webpack = require("webpack");
+
 module.exports = {
   baseUrl:
     process.env.NODE_ENV === "production" ? process.env.DEPLOY_URL_ROOT : "/",
   configureWebpack: {
-    devtool: "source-map"
+    devtool: "source-map",
+    plugins: [
+      new webpack.NormalModuleReplacementPlugin(
+        /element-ui[/\\]lib[/\\]locale[/\\]lang[/\\]zh-CN/,
+        "element-ui/lib/locale/lang/en"
+      )
+    ]
   }
 };
